44d18e65fd1103d67e106987ef653a2fd1f805a9
Eric Zhu <69171189+ericzhu105@users.noreply.github.com>
2023-08-25 01:43:55 -0400

    Updates SEO and profiles

--- src/views/team/teamData.js
+++ src/views/team/teamData.js
@@ -2,7 +2,7 @@ const teamData = [
     {
         "name": "Christian Elam",
         "title": "Founding General Partner",
-        "bio": "Christian is a 21-year-old investor and entrepreneur. He became the youngest person to attend Stanford for an exclusive computer science program at 10, launched successful apps on the App Store as a teenager, worked as a Senior iOS Engineer at 15 and has founded two startups one of which being venture backed. Christian has also graduated from UC Berkeley-Haas School of Business at 18. He has a track record in investing, transitioning from day trading to angel investing to being a VC & LP at Sutton Capital as well as the Gaingels. At the request of many founders and other investors, Christian founded Bachmanity Capital. Christian is not only focused on business but also dedicated to philanthropy, particularly in the areas of mental health and youth advocacy, making significant contributions to legislation and social impact initiatives.",
+        "bio": "Christian is the founding general partner at Bachmanity Capital focused on consumer, enterprise, & deeptech deals. Prior to founding Bachmanity, became the youngest person to attend Stanford for an exclusive computer science program at 10, launched successful apps on the App Store as a teenager, worked as a Senior iOS Engineer at 15 and has founded two startups one of which being venture backed. Christian has also graduated from UC Berkeley-Haas School of Business at 18. He has a track record in investing, transitioning from day trading to angel investing to being a VC & LP at Sutton Capital as well as the Gaingels. At the request of many founders and other investors, Christian founded Bachmanity Capital. Christian is not only focused on business but also dedicated to philanthropy, particularly in the areas of mental health and youth advocacy, making significant contributions to legislation and social impact initiatives.",
         "image": "site-pfp/elam.png",
         "twitter": "https://twitter.com/ChristianElam6",
         "linkedin": "https://www.linkedin.com/in/christian-elam-4b0a471a8/",
@@ -11,7 +11,7 @@ const teamData = [
     {
         "name": "Eric Zhu",
         "title": "General Partner",
-        "bio": "Eric is a 15 year old growth & technology focused entrepreneur. His expertise is in growth marketing, user acquisition, and operations. He has scaled one startup to 10k+ active users, and founded another startup, which was acquired after growing its waitlist to 49k+ users. Eric is an amazing networker, and has collaborated with several notable figures and executives. His inspiration for co-founding the fund stems from his experience as an operator, VC scout, and angel investor.",
+        "bio": "Eric Zhu is a general partner at Bachmanity Capital focused on enterprise software, data, & aerospace deals. Prior to joining Bachmanity, Eric co-founded an international AI hackathon backed by Tesla, and scaled a startup, amassing over 49,000 members and subsequently securing an acquisition. He was also one of the first employees at a climate fintech which was acquired for 7 figures in early April of 2021. Currently, he’s also the Founder at Aviato which accelerates deal flow for scouts and venture capitalists. Aviato's impressive trajectory is exemplified from having backers like 8VC, Sequoia Scout Fund, Soma Capital, Founder of Github, and more",
         "image": "site-pfp/eric.png",
         "twitter": "https://twitter.com/ericzhu105",
         "linkedin": "https://www.linkedin.com/in/ericzhu105/",
@@ -19,7 +19,7 @@ const teamData = [
     },
     {
         "name": "Chuck Figueroa",
-        "title": "Partner",
+        "title": "Venture Partner",
         "bio": "Chuck is a corporate strategist, sports endorsement agent and angel investor. He received his BA from the University of California, Los Angeles. He began his career in Public Relations which led him into the sports industry and has executed global partnerships with the elite NBA,NFL and Olympic athletes. Chuck currently sits on the board and advises a global ecosystem in media, technology and venture capital at the intersection of innovation and legacy.",
         "image": "site-pfp/chuck.png",
         "twitter": "https://twitter.com/Chuck_LA",
@@ -28,7 +28,7 @@ const teamData = [
     },
     {
         "name": "Kristin Stueben",
-        "title": "Partner",
+        "title": "Venture Partner",
         "bio": "Kristin is a serial entrepreneur, angel investor, creative director, and influencer. She cofounded Maxus Nails, a very successful proprietary nail product winning the Allure Beauty Award, featured in Vogue, InStyle, ELLE, Cosmopolitan, and more. She cofounded Cerebral Agency focusing on celebrity endorsements, tech, and venture capital. Kristin is also on the advisory board of We Empower Women.",
         "image": "site-pfp/kris.png",
         "twitter": "",
@@ -40,8 +40,8 @@ const teamData = [
         "title": "Advisor",
         "bio": "Gleb Chuvpilo is a Ukrainian-American serial entrepreneur and early-stage venture capital investor, with a Master's degree from the MIT Computer Science and Artificial Intelligence Lab and an MBA in Finance and Strategic Management from The Wharton School at the University of Pennsylvania. Gleb started his career as a quantitative trader at Lehman Brothers and Goldman Sachs, and spent years investing alongside Peter Thiel at his global macro hedge fund, Thiel Macro. Gleb was an early engineer at Palantir, and then co-founded several technology startups, including Authy (Y Combinator class 2012, acquired by Twilio), Ride (funded by TPG Growth, acquired by Enterprise), Pager (funded by NEA and Lux), and CryptoLeague (stealth). Gleb is a Chartered Financial Analyst (CFA) and an Energy Risk Professional (ERP), as well as a graduate of the MIT Sloan Financial Technology program and the Reforge Growth Series with Andrew Chen. Gleb is proud to serve on the board of MIT delta v, an educational accelerator.",
         "image": "site-pfp/gleb.png",
-        "twitter": "",
-        "linkedin": "",
+        "twitter": "https://twitter.com/chuvpilo",
+        "linkedin": "https://www.linkedin.com/in/chuvpilo/",
         "slug": "/investors/Gleb-Chuvpilo"
     },
     {
@@ -50,7 +50,7 @@ const teamData = [
         "bio": "Patrick Morselli currently serves as the COO of Resilience Labs, a high growth venture backed startup that’s modernizing how clinicians work together to provide quality mental health care. Patrick was amongst the earliest employees at Uber & WeWork, serving as the Head of Global Expansion for both. He has worked with 15+ ventures across AI, consumer, prop-tech, and health-tech, impacting growth exponentially by executing seamlessly and attracting top talent. Patrick has invested in over 80 companies to date and is the founder & partner of Superlumnial Partners.",
         "image": "site-pfp/patrick.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/patrickmorselli/",
         "slug": "/investors/Patrick-Morselli"
     },
     {
@@ -59,7 +59,7 @@ const teamData = [
         "bio": "Dr. Joel Palathinkal is an active investor and entrepreneur. He's the CEO of Sutton Capital and an LP / Mentor to both emerging and institutional investment managers. Sutton Capital invests in opportunities focused on Real Estate, B2B SaaS, Deep Tech & Clean Energy / Infrastructure. Early in Joel’s career, he ran technology product innovation in Fintech, Artificial Intelligence, & for the Dept. of Defense. He completed his Ph.D in Modeling & Simulation while he was building flight simulators for the NAVY & DOD.",
         "image": "site-pfp/joel.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/joelpalathinkal/",
         "slug": "/investors/Joel-Palathinkal"
     },
     {
@@ -68,7 +68,7 @@ const teamData = [
         "bio": "Alex Cornell du Houx was elected to the Maine House of Representatives and quickly became one of its most active members. Sponsoring and co-sponsoring 155 bills his first term, and 136 his second term. In total sponsoring 14 successful pieces of legislation and co-sponsoring 98 successful bills. Cornell du Houx founded Elected Officials to Protect America and Code Blue Water Security Conflicts and Solutions. He won the REED award for the best national Public Affairs Campaign for his work founding Operation Free, a coalition of veterans combating climate change with the Truman Project. Cornell du Houx participated and led State Department delegations to Southeast Asia to foster government-to-government relations, is a Huffington Post contributor, a Kentucky Colonel, a senior advisor and instructor to the Veterans Campaign, a member of the Global Advisory Council for the American Veteran’s Committee, Partner with the Truman National Security Project, a board member with the Solon Center for Research and Publishing, and founded the Maine New Leaders Council (NLC). Cornell du Houx teaches at the University of San Francisco and his trainings are requested worldwide. Alex is a certified trainer with NLC, The Truman National Security Project, and a leadership coach with ChoiceCenter Leadership University. Cornell du Houx served in the Marine Corps infantry as an assultman, for seven years and has deployed to Iraq and Afghanistan. He is currently a Public Affairs Officer with the Navy Reserve and is the President of Elected Officials to Protect America and Code Blue Water Security Solutions. Cornell du Houx graduated from Bowdoin College with Government and Legal Studies and Theater and holds a Masters in Public Leadership from the University of San Francisco.",
         "image": "site-pfp/alex.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/cornellalex/",
         "slug": "/investors/Alex-Cornell"
     },
     {
@@ -77,7 +77,7 @@ const teamData = [
         "bio": "Founder of LvlUp Ventures in Los Angeles and previously a Co-founder of Quake Capital, a venture capital fund and accelerator with offices in NYC, Los Angeles, and Austin. Quake has invested in 200+ companies with 9 portfolio exits to date. Named top 10 VCs in the world by Forbes and the #2 investor in female-founded companies by CrunchBase and most recently named #1 in worldwide diversity-based investments by BetaBoom",
         "image": "site-pfp/brandon.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/brandontmaier/",
         "slug": "/investors/Brandon-Maier"
     },
     {
@@ -86,7 +86,7 @@ const teamData = [
         "bio": "Ian Arden is the founder of Mempool Ventures and is an Abu Dhabi based AI & Web 3 investor. He is a serial entrepreneur and advisor to 20+ brands and has accelerated over 300 global blockchain ventures. Ian is a former developer with a $130M exit.",
         "image": "site-pfp/ian.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/ian-arden/?originalSubdomain=ae",
         "slug": "/investors/Ian-Arden"
     },
     {
@@ -95,7 +95,7 @@ const teamData = [
         "bio": "Conor Patton has a wealth of international experience in M&A, Capital Markets, and Capital Raising across various sectors, including Real Estate and Technology. His direct and formal approach is particularly evident in his focus on investor relations, deal origination, execution, and management, and his knowledge of Private and Public markets is unparalleled. Conor has built a strong reputation in the industry, thanks to his well-connected network of clients and partners across Europe and the Middle East, which has been an integral part of his success in raising over $30 million and executing capital market deals worth over $200 million.",
         "image": "site-pfp/conor.png",
         "twitter": "",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/conor-patton-910401167/?originalSubdomain=uk",
         "slug": "/investors/Conor-Patton"
     }
 ];
